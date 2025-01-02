5402f3594aa61e0bcf04d9269c7884cdebe3ce5c
Mark Peters <mapeters101@earthlink.net>
2025-01-02 00:56:41 -0500

    Setup.

--- main.py
+++ main.py
@@ -1,2 +1,60 @@
-print("Hi")
-print("Hi!")
+import random
+
+word_list = ["python", "hangman", "programming", "computer", "algorithm", "software", "bethmedia", "furmilayo"]
+
+chosen_word = random.choice(word_list)
+
+guesses = []
+max_attempts = 6
+attempts = 0
+
+hangman_stages = [
+    """
+    -----
+    |   |
+        |
+        |
+        |
+        |    
+    """,
+    """
+    -----
+    |   |
+    O   |
+        |
+        |
+        |    
+    """,
+    """
+    -----
+    |   |
+    O   |
+    |   |
+        |
+        |    
+    """,
+    """
+    -----
+    |   |
+    O   |
+   /|   |
+        |
+        |    
+    """,
+    """
+    -----
+    |   |
+    O   |
+   /|\  |
+   /    |
+        |    
+    """,
+    """
+    -----
+    |   |
+    O   |
+   /|\  |
+   / \  |
+        |    
+    """
+]
